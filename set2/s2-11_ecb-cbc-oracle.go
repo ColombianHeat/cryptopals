687ee4fcd347e2b33026f1e3ec23aa3e7a417aa5
Alan Gilchrist <agilchri@UWATERLOO.CA>
2025-01-17 16:01:38 -0500

    17-Jan-2025

--- set2/s2-11_ecb-cbc-oracle.go
+++ set2/s2-11_ecb-cbc-oracle.go
@@ -1,1 +1,65 @@
 package set_two
+
+import (
+	"cryptopals/shared"
+	"fmt"
+	"math/rand"
+)
+
+// RandAESKey returns a randomly generated AES key of length blockSize
+// The key is suitable for AES encryption and decryption in ECB and CBC modes.
+func RandAESKey(blockSize int) []byte {
+	key := make([]byte, blockSize)
+	for i := 0; i < blockSize; i++ {
+		key[i] = byte(rand.Intn(256))
+	}
+	return key
+}
+
+func EncryptAESInCBC(plainText []byte, key string) []byte {
+	blockSize := len([]byte(key))
+	plainText = ImplementPKCSPadding(plainText, blockSize)
+	iv := make([]byte, blockSize) // initialization vector (all zeroes)
+	output := make([]byte, len(plainText))
+
+	for bs, be := 0, blockSize; bs < len(plainText); bs, be = bs+blockSize, be+blockSize {
+		plainTextBytesBlock := plainText[bs:be]
+		// XOR with IV
+		plainTextBytesBlock = shared.XorByteVectors(plainTextBytesBlock, iv)
+		// Encrypt with ECB
+		cipherTextBytesBlock := EncryptAesInECB(plainTextBytesBlock, key)
+		// Update output
+		copy(output[bs:be], cipherTextBytesBlock)
+		// Update IV for next iteration
+		iv = cipherTextBytesBlock
+	}
+	
+	return output
+}
+
+func ECBorCBCOracle(plainText string, blockSize int) []byte {
+	key := RandAESKey(blockSize)
+	mode := rand.Intn(2) // 0 = ECB, 1 = CBC
+
+	nAppended := rand.Intn(5) + 5 // 5 to 10 bytes appended
+	prefix := make([]byte, nAppended)
+	for i := 0; i < nAppended; i++ {
+		prefix[i] = byte(rand.Intn(256))
+	}
+	suffix := make([]byte, nAppended)
+	for i := 0; i < nAppended; i++ {
+		suffix[i] = byte(rand.Intn(256))
+	}
+	plainText = string(prefix) + plainText + string(suffix)
+
+	if mode == 0 {
+		fmt.Println("Challenge 11: ECB mode")
+		return EncryptAesInECB([]byte(plainText), string(key)) // FIXME: This throws an error (line 20). Why?
+	} else if mode == 1 {
+		fmt.Println("Challenge 11: CBC mode")
+		return EncryptAESInCBC([]byte(plainText), string(key))
+	}
+
+
+	return []byte{}
+}
--- set2/s2-11_ecb-cbc-oracle_test.go
+++ set2/s2-11_ecb-cbc-oracle_test.go
@@ -1,1 +1,30 @@
 package set_two
+
+import (
+	"cryptopals/shared"
+	"encoding/base64"
+	"strings"
+	"testing"
+)
+
+func TestEncryptAESInCBC(t *testing.T) {
+	input := shared.ImportTxtFile("./../set1/data/funkymusic.txt")
+	gotBytes := EncryptAESInCBC([]byte(input), "YELLOW SUBMARINE")
+	gotStr := base64.StdEncoding.EncodeToString(gotBytes)
+
+	wantNewlines := shared.ImportTxtFile("./data/s2-10.txt")
+	want := strings.Replace(wantNewlines, "\n", "", -1)
+
+	if gotStr != want {
+		t.Errorf("\ngot %s,\nwant %s", gotStr, want)
+	}
+}
+
+func TestECBorCBCOracle(t *testing.T) {
+	got := ECBorCBCOracle("Sample input. How is everybody doing on this fine January afternoon?", 16)
+	want := make([]byte, 16)
+
+	if !shared.CompareByteArrs(got, want) {
+		t.Errorf("\ngot %v,\nwant %v", got, want)
+	}
+}
